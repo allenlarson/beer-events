2ef3ccabb47b0d93752f792ffe2c4d99f985927b
Allen Larson <127446190+allenlarson@users.noreply.github.com>
2024-04-08 23:46:52 -0400

    Added all fields to event page

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -237,28 +237,53 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                       {...field}
                       className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
                     />
+                    <FormField
+                      control={form.control}
+                      name="isFree"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="flex-center items-center">
+                              <label
+                                htmlFor="isFree"
+                                className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                              >
+                                Free Ticket
+                              </label>
+                              <Checkbox
+                                id="isFree"
+                                className="mr-2 h-5 w-5 border-2 border-primary-500"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
                   </div>
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
-            name="isFree"
+            name="url"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="w-full">
                 <FormControl>
-                  <div className="flex-center items-center">
-                    <label
-                      htmlFor="isFree"
-                      className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                      Free Ticket
-                    </label>
-                    <Checkbox
-                      id="isFree"
-                      className="mr-2 h-5 w-5 border-2 border-primary-500"
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/link.svg"
+                      alt="link"
+                      width={24}
+                      height={24}
+                    />
+                    <Input
+                      placeholder="External URL"
+                      {...field}
+                      className="input-field"
                     />
                   </div>
                 </FormControl>
@@ -268,7 +293,14 @@ const EventForm = ({ userId, type }: EventFormProps) => {
           />
         </div>
 
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+        >
+          {form.formState.isSubmitting ? 'Submitting...' : `${type} Event`}
+        </Button>
       </form>
     </Form>
   );
